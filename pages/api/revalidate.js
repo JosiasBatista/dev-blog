f6211b8ed39d6e8d31e4815d966d6cda2cec32aa
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-11-26 17:18:30 -0300

    Parsing the requesto body

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,7 +4,8 @@ export default async function handler(req, res) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  const modifiedPosts = req.body.payload.commits[0].modified;
+  const reqBody = JSON.parse(req.body)
+  const modifiedPosts = reqBody.payload.commits[0].modified;
 
   try {
     // this should be the actual path not a rewritten path
