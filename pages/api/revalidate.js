6f86f3051c77b6133a341093ef73899abce6e027
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-11-26 14:07:09 -0300

    Revalidating path adjust

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -9,14 +9,12 @@ export default async function handler(req, res) {
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    modifiedPosts.forEach(async postName => {
-      await res.revalidate(`/posts/${postName.replace(/\.md$/, '')}`)
-    });
+    await res.revalidate(`/posts/${modifiedPosts[0].replace(/\.md$/, '')}`)
 
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send(err)
   }
 }
