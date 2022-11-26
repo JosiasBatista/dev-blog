901d050e340c74e87a62cbb2adf3f92f3eadda68
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-11-26 14:02:27 -0300

    Revalidating path adjust

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,12 +4,15 @@ export default async function handler(req, res) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  console.log(req.body);
+  const modifiedPosts = req.body.commits[0].modified;
 
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    // await res.revalidate('/path-to-revalidate')
+    modifiedPosts.forEach(async postName => {
+      await res.revalidate(`/posts/${postName.replace(/\.md$/, '')}`)
+    });
+
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
