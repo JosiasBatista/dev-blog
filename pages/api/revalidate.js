08b96086fe523fc72702b245d65a8cdbac472903
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-11-28 20:37:45 -0300

    Adjusting problem with revalidation

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
   try {
     // this should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate(`/posts/${modifiedPosts[0].replace(/\.md$/, '')}`)
+    if (modifiedPosts.length > 0) {
+      await res.revalidate(`/posts/${modifiedPosts[0].replace(/\.md$/, '')}`)
+    }
 
     return res.json({ revalidated: true })
   } catch (err) {
