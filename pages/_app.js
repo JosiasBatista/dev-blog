08137b631dbeec873bc8bf927d5bb37e3ca4c2a3
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2022-12-04 20:41:05 -0300

    Adding google ads script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Script from 'next/script';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script id="Adsense-id" data-ad-client="ca-pub-7346360878393267"  async strategy="afterInteractive"
+        onError={ (e) => { console.error('Script failed to load', e) }}src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Component {...pageProps} />;
+    </>
+  )
 }
