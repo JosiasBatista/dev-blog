92e2b811bc09fd7f22c2ea9130297266f5987f39
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2023-01-16 20:56:48 -0300

    Adjusting google ads script

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,6 @@ import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Script id="Adsense-id" data-ad-client="ca-pub-7346360878393267"  async strategy="afterInteractive"
-        onError={ (e) => { console.error('Script failed to load', e) }}src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
-      <Component {...pageProps} />;
-    </>
+    <Component {...pageProps} />
   )
 }
